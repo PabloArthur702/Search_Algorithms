9c3565bf02c48f4cf39c6de2ccd5afc7daae4e5a
pabloart <pablo.filho.702@ufrn.edu.com>
2024-12-12 00:30:16 -0300

    quickSort implemented and tested

--- quickSort.go
+++ quickSort.go
@@ -1,19 +1,38 @@
 package main
 
-import (
+import(
 	"fmt"
 )
 
-//Função de particionamento
-func partitioning (array []int, ini int, fim int)){
-	
+//Conquista
+func partitioning (v []int, ini int, fim int) int{
+	pivot := v[fim]
+	pIndex := ini
+	for i := ini; i < fim; i++{
+		if v[i] <= pivot{
+			aux := v[pIndex]
+			v[pIndex] = v[i]
+			v[i] = aux	
+			pIndex += 1
+		}
+	}
+	aux := v[pIndex]
+	v[pIndex] = pivot
+	v[fim] = aux
+	return pIndex	
 }
 
 //Divisão
-func quickSort(array []int, ini int, fim int){
-	if ini < fim{
-		partition := partitioning(array, ini, fim)
+func quickSort(v []int, ini int, fim int){
+	if fim > ini{
+		pivotIndex := partitioning(v, ini, fim)
+		quickSort(v, ini, pivotIndex-1)
+		quickSort(v, pivotIndex+1, fim)
 	}
 }
 
-//Conquista
+func main(){
+	v := [] int{8, 7, 5, 3, 2, 3, 4, 6, 9, 10} 
+	quickSort(v, 0, len(v)-1)
+	fmt.Println(v)
+}
